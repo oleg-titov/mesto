d9ee28ea598cc09f3e56c36a669bfac944a320f3
Oleg Titov <dersteinalchimist@yandex.ru>
2022-12-23 13:11:30 +0300

    Check. Loop 2. Issue A

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,44 +1,60 @@
-// Переменные и константы
-const editButton = document.querySelector('.profile__edit-button'); // Кпопка редактировать профиль
-const closeEditForm = document.querySelector('.popup__close'); // Ссылка на закрытие окна редактирования профиля
-const editForm = document.querySelector('.popup'); // Модальное окно
-const buttonSubmit = document.querySelector('.popup__button-submit'); // Кнопка "сохранить"
+/* Константы */
+const buttonEdit = document.querySelector('.profile__edit-button'); // Кпопка редактирования профиля
+const formEditClose = document.querySelector('.popup__close'); // Ссылка на закрытие окна редактирования профиля
+const formEdit = document.querySelector('.popup'); // Модальное окно
+const buttonLike = document.querySelectorAll('.elements__like'); // Выбор всех кнопок like
 
-// Блок заполнения полей ввода в модальном окне значениями из HTML
-let profileName = document.querySelector('.profile__title').innerHTML;
-let profileDescription = document.querySelector('.profile__description').innerHTML;
-document.querySelector('#name').value = profileName 
-document.querySelector('#about').value = profileDescription 
+/* Функции */
 
 // Функция добавления класса "видимости" модальному окну
 function addEditForm() {
-	editForm.classList.add('popup_visible');
+  formEdit.classList.add('popup_opened');
 }
-
 // Функция удаления класса "видимости" модальному окну
 function removeEditForm() {
-	editForm.classList.remove('popup_visible');
+  formEdit.classList.remove('popup_opened');
 }
 
-// Функция изменения отображения значка like по нажатию на него
-function like(x) {
-    x.classList.toggle('elements__like_active');
-}
-
-// Функция записи в HTML значения из полей ввода в модальном окне
-function changeNameAndAbout() {
-    document.querySelector('.profile__title').innerHTML = document.querySelector('#name').value
-    document.querySelector('.profile__description').innerHTML = document.querySelector('#about').value
-}
+/* События */
 
 // Событие: нажатие на кнопку редактирования
-editButton.addEventListener('click', addEditForm);
+buttonEdit.addEventListener('click', function () {
+  // Форма в DOM
+  let formElement = document.querySelector('.popup__edit-form');
+  
+  // Поля формы в DOM
+  let nameElement = document.querySelector('.profile__title');
+  let jobElement = document.querySelector('.profile__description');
+  nameInput =  document.querySelector('#name');
+  jobInput = document.querySelector('#about');
+  nameInput.value = nameElement.textContent;
+  jobInput.value = jobElement.textContent;
+
+  // Обработчик «отправки» формы
+  function handleFormSubmit(evt) {
+    evt.preventDefault(); // Отмена стандартной отправки формы
+
+    // Значение полей jobInput и nameInput из свойства value
+    let nameInputValue = nameInput.value;
+    let jobInputValue = jobInput.value;
+    // Вставка новых значения с помощью textContent
+    nameElement.textContent = nameInputValue;
+    jobElement.textContent = jobInputValue;
+  }
+
+  addEditForm(); // Отображение модального окна
+
+  // Прикрепление обработчика к форме:
+  // он следит за событием “submit” - «отправка»
+  formElement.addEventListener('submit', handleFormSubmit);
+});
 
 // Событие: нажатие на крест модального окна
-closeEditForm.addEventListener('click', removeEditForm);
+formEditClose.addEventListener('click', removeEditForm);
 
-// Событие: нажатие на кнопку сохранения
-buttonSubmit.addEventListener('click', function buttonSubmitPress() {
-    changeNameAndAbout();
-    removeEditForm();
-});
+// Событие: нажатие на кнопку like со встроенной функций смены класса
+buttonLike.forEach(function (btn) {
+  btn.addEventListener('click', function () {
+    btn.classList.toggle("elements__like_active");
+  })
+}); 
